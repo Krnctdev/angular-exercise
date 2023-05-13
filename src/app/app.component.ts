c20cea2d5954540696bbdfb5c262a4b9ce06b34a
kcifuenteso <kcifuentes@avla.com>
2023-05-13 16:48:59 -0400

    base project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-exercise';
+  data: any[] = [];
+
+  constructor(private http: HttpClient) { 
+    /* this.http.get('https://api.spacexdata.com/v4/rockets').subscribe(data => {
+      console.log(data);
+    }); */
+  }
+
+  ngOnInit() {
+    this.http.get('https://api.spacexdata.com/v4/rockets').subscribe(data => {
+      this.data = data as any[];
+      console.log(data);
+    });
+  }
 }
